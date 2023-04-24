0e07895a4ff4d3afb797499f21860d8604e0d69d
Florescu Cristian <qflorescucristian@gmail.com>
2023-04-24 16:44:09 +0300

    Problema 25

--- recapitulare-algoritmi-vectori/fisa-vectori-2.h
+++ recapitulare-algoritmi-vectori/fisa-vectori-2.h
@@ -22,20 +22,22 @@ struct Polinom {
 	void afisare() {
 		cout << litera << "(x) = ";
 		for (int i = 0; i < grad + 1; i++) {
-			if (X[i].coeficient < 0) {
-				cout << "- ";
-			}
-			else {
-				if (i != 0) {
-					cout << "+ ";
+			if (X[i].coeficient != 0) {
+				if (X[i].coeficient < 0) {
+					cout << "- ";
 				}
-			}
-			cout << abs(X[i].coeficient);
-			if (i < grad - 1) {
-				cout << "X^" << X[i].putere << " ";
-			}
-			if (i == grad - 1) {
-				cout << "X ";
+				else if (i != 0) {
+					cout << "+ ";
+				} 
+				if (abs(X[i].coeficient) != 1 || X[i].putere == 0) {
+					cout << abs(X[i].coeficient);
+				}
+				if (i < grad - 1) {
+					cout << "X^" << X[i].putere << " ";
+				}
+				else if (i == grad - 1) {
+					cout << "X ";
+				}
 			}
 		}
 		cout << endl;
@@ -60,24 +62,40 @@ int maxProblema25(int n, int m) {
 	if (n > m) {
 		return n;
 	}
-	return m;
+	return m;	
 }
 
-void sumaPolinoame(Polinom A, Polinom B, Polinom& R) {
+void sumaPolinoame(Polinom A, Polinom B) {
+	Polinom R;
+	strcpy(R.litera, "Suma");
 	R.grad = maxProblema25(A.grad, B.grad);
 	for (int i = 0; i < R.grad + 1; i++) {
 		R.X[i].putere = R.grad - i;
 	}
 	for (int i = 0; i < A.grad + 1; i++) {
-		R.X[A.X[i].putere].coeficient += A.X[i].coeficient;
+		R.X[R.grad - A.X[i].putere].coeficient += A.X[i].coeficient;
 	}
 	for (int i = 0; i < B.grad + 1; i++) {
-		R.X[B.X[i].putere].coeficient += B.X[i].coeficient;
+		R.X[R.grad - B.X[i].putere].coeficient += B.X[i].coeficient;
 	}
+	R.afisare();
 }
 
-void produsPolinoame(Polinom A, Polinom B, Polinom& R) {
+void produsPolinoame(Polinom A, Polinom B) {
+	Polinom R;
+	strcpy(R.litera, "Produs");
 	R.grad = A.grad + B.grad;
+	for (int i = 0; i < R.grad + 1; i++) {
+		R.X[i].putere = R.grad - i;
+	}
+	for (int i = 0; i < A.grad + 1; i++) {
+		for (int j = 0; j < B.grad + 1; j++) {
+			int k = A.X[i].putere + B.X[j].putere;
+			int p = A.X[i].coeficient * B.X[j].coeficient;
+			R.X[R.grad - k].coeficient += p;
+		}
+	}
+	R.afisare();
 }
 
 void problema25() {
@@ -85,17 +103,19 @@ void problema25() {
 	citireProblema25(P, Q);
 	P.litera[0] = 'P', Q.litera[0] = 'Q';
 	
-	// Suma polinoamelor.
+	// Afisarea polinomelor
 
-	Polinom Suma;
-	strcpy(Suma.litera, "Suma");
-	sumaPolinoame(P, Q, Suma);
-	Suma.afisare();
+	P.afisare();
+	Q.afisare();
 
-	// Produsul polinoamelor.
+	cout << endl;
 
-	Polinom Produs;
-	strcpy(Produs.litera, "Produs");
-	produsPolinoame(P, Q, Produs);
-	Produs.afisare();
+	// Suma si produsul polinoamelor.
+	
+	sumaPolinoame(P, Q);
+
+	produsPolinoame(P, Q);
 }
+
+// Problema 26
+// 
